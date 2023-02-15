a1dd39d9a2535bb8fa5fdd278e7680a999147f16
Vadzimdi <vadzimdi@gmail.com>
2023-02-16 00:08:15 +0300

    added Error wiki/Title

--- wiki2/encyclopedia/views.py
+++ wiki2/encyclopedia/views.py
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def convert_md_to_html(md):
+    s = Markdown(md)
+    return s
+
+
+def entry(request, title):
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+            
