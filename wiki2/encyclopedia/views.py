b4d0b12b13cc6fd64aa5676502d5a60924922d5b
Vadzimdi <vadzimdi@gmail.com>
2023-02-16 00:30:48 +0300

    added wiki/title(workable)

--- wiki2/encyclopedia/views.py
+++ wiki2/encyclopedia/views.py
@@ -3,18 +3,30 @@ from markdown2 import Markdown
 from . import util
 
 
+def convert_md_to_html(md):
+    res = util.get_entry(md)
+    s = Markdown()   
+    return s.convert(res)
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-def convert_md_to_html(md):
-    s = Markdown(md)
-    return s
 
 
 def entry(request, title):
     if util.get_entry(title) == None:
         return render(request, "encyclopedia/error.html")
     else:
+        all_md = util.list_entries()
+        for md in all_md:
+            if title.lower() in md.lower():
+                title = md
+        res = convert_md_to_html(title)
+        return render(request, "encyclopedia/converting.html", {
+            "title": title,
+            "res": res
+        })
+
             
