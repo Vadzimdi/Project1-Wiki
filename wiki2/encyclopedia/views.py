56c3a89d98da61e45c0a51ef10f07fb9fdf4555b
Vadzimdi <vadzimdi@gmail.com>
2023-02-16 22:55:41 +0300

    added search.html

--- wiki2/encyclopedia/views.py
+++ wiki2/encyclopedia/views.py
@@ -4,7 +4,10 @@ from . import util
 
 
 def convert_md_to_html(md):
-    res = util.get_entry(md)
+    if util.get_entry(md) == None:
+        return None
+    else:
+        res = util.get_entry(md)    
     s = Markdown()   
     return s.convert(res)
 
@@ -29,4 +32,31 @@ def entry(request, title):
             "res": res
         })
 
-            
+
+def search(request):
+    if request.method == "POST":
+        look_for = request.POST["q"]
+        res = convert_md_to_html(look_for)
+        if res:            
+            return render(request, "encyclopedia/converting.html", {
+                "title": res,
+                "res": res
+            })
+        else:
+            all_possible = []
+            all_md = util.list_entries()
+            for md in all_md:
+                if look_for.lower() in md.lower():
+                    all_possible.append(md)
+            if all_possible:
+                return render(request, "encyclopedia/search.html", {
+                "entries": all_possible
+            })
+            else:
+                return render(request, "encyclopedia/error.html")
+
+
+
+                
+
+
